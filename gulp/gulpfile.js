72f0db69783f0deb2a368e8c380d354dc89809db
ido barnea <barbareshet@gmail.com>
2017-05-04 23:58:03 +0300

    other events setion

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -7,9 +7,7 @@ var sass = require('gulp-sass');
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: {
-            baseDir: "./"
-        },
+        server: "..",
         notify: false
     });
 
