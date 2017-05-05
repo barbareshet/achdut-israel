79fe78fa1b4ec3b4157e8609d2dfb9579e99b318
ido barnea <barbareshet@gmail.com>
2017-05-05 15:04:17 +0300

    events

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -5,18 +5,25 @@ var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 
 gulp.task('serve', ['sass'], function() {
+    //timestamp
+    var d = new Date();
+    var ts = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
 
+    console.log("Last update at " + ts);
     browserSync.init({
         server: "..",
         notify: false
     });
 
     gulp.watch(path +"/assets/scss/*.scss", ['sass']);
+    gulp.watch(path + "/*.html",['html']);
     // gulp.watch(path +"/**/*", browserSync.reload);
 });
-
+gulp.task('html', function(){
+   console.log('You Have Changed an html file');
+});
 gulp.task('sass', function() {
-    return gulp.src(path +"/assets/scss/*.scss")
+    return gulp.src(path + "/assets/scss/*.scss")
         .pipe(plumber())
         .pipe(sass.sync().on('error', sass.logError))
         .pipe(sass())
